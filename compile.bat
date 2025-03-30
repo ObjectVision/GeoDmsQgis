0c6b6e3ef3f7c9580fe14f0983b32767ad317d5d
eoudejans <eoudejans@objectvision.nl>
2025-03-30 17:03:48 +0200

    Updated compile.bat script to copy repo to correct location.

--- compile.bat
+++ compile.bat
@@ -4,5 +4,5 @@ call C:/OSGeo4W/bin/o4w_env.bat
 
 C:/OSGeo4W/apps/Python312/Scripts/pyrcc5.exe -o resources.py resources.qrc
 
-xcopy C:\dev\geodms_qgis\geodms C:\OSGeo4W\apps\qgis\python\plugins\geodms /E /H /C /I
+xcopy C:\dev\geodms_qgis\GeoDmsQgis C:\OSGeo4W\apps\qgis\python\plugins\geodms /E /H /C /I
 
